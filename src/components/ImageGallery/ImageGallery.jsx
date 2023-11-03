e723deab2cadaa996da1ec6133fc64931fccccdc
ArtemCherchuk <250artem250@gmail.com>
2023-11-03 11:31:24 +0100

    upd

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,7 +2,6 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css';
 
 export const ImageGallery = ({ arrayData, openModal }) => {
-  // console.log(arrayData);
   return (
     <ul className={css.ImageGallery}>
       {arrayData.length !== 0 &&
